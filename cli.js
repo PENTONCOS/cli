c77e7da56b2bf913ff5b5e532f0a0e35b30800e0
PENTONCOS <515327098@qq.com>
2021-06-22 20:56:56 +0800

    feat:init done

--- cli.js
+++ cli.js
@@ -6,6 +6,9 @@
 // 具体就是通过 chmod 755 cli.js 实现修改
 
 const inquirer = require('inquirer')
+const path = require('path')
+const fs = require('fs')
+const ejs = require('ejs')
 
 inquirer.prompt([
   {
@@ -15,8 +18,24 @@ inquirer.prompt([
     default: 'my-app' // 默认值
   }
 ]).then(answers => {
-  // 打印互用输入结果
-  console.log(answers)
+  // 模版文件目录
+  const destUrl = path.join(__dirname, 'templates');
+  // 生成文件目录
+  // process.cwd() 对应控制台所在目录
+  const cwdUrl = process.cwd();
+  // 从模版目录中读取文件
+  fs.readdir(destUrl, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      // 使用 ejs 渲染对应的模版文件
+      // renderFile（模版文件地址，传入渲染数据）
+      ejs.renderFile(path.join(destUrl, file), answers).then(data => {
+        console.log(data)
+        // 生成 ejs 处理后的模版文件
+        fs.writeFileSync(path.join(cwdUrl, file), data)
+      })
+    })
+  })
 })
 
 
